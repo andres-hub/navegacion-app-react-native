1c6a0873bbd4b981e2317ac8906362676d963c50
AndresCL <andrescalderoncl@gmail.com>
2022-08-24 10:07:14 -0500

    ok video 110 navegacion

---
+++ src/screens/Pagina2Screen.tsx
@@ -0,0 +1,27 @@
+import { useNavigation } from '@react-navigation/native'
+import React, { useEffect } from 'react'
+import { Button, Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+import { StackScreenProps } from '@react-navigation/stack';
+
+interface Props extends StackScreenProps<any, any>{}
+
+export const Pagina2Screen = ( {navigation} :Props) => {
+  // Solo para ios
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Hola mundo',
+      headerBackTitle: 'Atras'
+    })
+  }, [])
+  
+  return (
+    <View style={styles.globalMargin}>
+        <Text style={styles.title}> Pagina2Screen </Text>
+        <Button
+        title='Ir pagina 3'
+        onPress={()=> navigation.navigate('Pagina3Screen')}
+        />
+    </View>
+  )
+}
